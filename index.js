f188f966c1804235c23bed800071e62531dcc956
nicholas-apolinario03 <50881682+nicholas-apolinario03@users.noreply.github.com>
2023-10-21 00:25:52 -0300

    Update index.js

--- index.js
+++ index.js
@@ -20,24 +20,12 @@ app.get('/getData', async (req, res) => {
     const collection1 = client.db('data_sensor').collection('luz_ambiente');
     const collection2 = client.db('data_sensor').collection('pressao');
     const collection3 = client.db('data_sensor').collection('temperatura_ar');
-    const collection4 = client.db('data_sensor').collection('temperatura_solo');
-    const collection5 = client.db('data_sensor').collection('umidade_ar');
-    const collection6 = client.db('data_sensor').collection('umidade_solo');
-    const collection7 = client.db('data_sensor').collection('vento_direção');
-    const collection8 = client.db('data_sensor').collection('vento_velocidade');
-    const collection9 = client.db('data_sensor').collection('heigth');
+  
     // Consulta para recuperar dados do MongoDB
     const data1 = await collection1.find({}).toArray();
     const data2 = await collection2.find({}).toArray();
     const data3 = await collection3.find({}).toArray();
-    const data4 = await collection4.find({}).toArray();
-    const data5 = await collection5.find({}).toArray();
-    const data6 = await collection6.find({}).toArray();
-    const data7 = await collection7.find({}).toArray();
-    const data8 = await collection8.find({}).toArray();
-    const data9 = await collection9.find({}).toArray();
-
-
+    
     // Feche a conexão com o MongoDB
     await client.close();
 
@@ -47,23 +35,11 @@ app.get('/getData', async (req, res) => {
     const values2 = data2.map(item => item.data);
     const labels3 = data3.map(item => item.date);
     const values3 = data3.map(item => item.data);
-    const labels4 = data4.map(item => item.date);
-    const values4 = data4.map(item => item.data);
-    const labels5 = data5.map(item => item.date);
-    const values5 = data5.map(item => item.data);
-    const labels6 = data6.map(item => item.date);
-    const values6 = data6.map(item => item.data);
-    const labels7 = data7.map(item => item.date);
-    const values7 = data7.map(item => item.data);
-    const labels8 = data8.map(item => item.date);
-    const values8 = data8.map(item => item.data);
-    const labels9 = data9.map(item => item.date);
-    const values9 = data9.map(item => item.data);
-    
+   
 
 
     // Responda com os dados em formato JSON
-    res.json({ labels1,values1, labels2,values2, labels3,values3, labels4,values4, labels5,values5, labels6,values6, labels7,values7, labels8,values8, labels9, values9});
+    res.json({ labels1,values1, labels2,values2, labels3,values3});
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Erro ao buscar dados no MongoDB' });
