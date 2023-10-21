3d40363397f720cd55f59f673e291434fd9eb1cc
nicholas-apolinario03 <50881682+nicholas-apolinario03@users.noreply.github.com>
2023-10-21 01:17:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,30 +16,32 @@ app.get('/getData', async (req, res) => {
   try {
     const client = new MongoClient(process.env.MONGODB_CONNECT_URI);
     await client.connect();
-    // Acesse a coleção desejada
-    const collection1 = client.db('data_sensor').collection('luz_ambiente');
-    const collection2 = client.db('data_sensor').collection('pressao');
-    const collection3 = client.db('data_sensor').collection('temperatura_ar');
-  
-    // Consulta para recuperar dados do MongoDB
-    const data1 = await collection1.find({}).toArray();
-    const data2 = await collection2.find({}).toArray();
-    const data3 = await collection3.find({}).toArray();
-    
-    // Feche a conexão com o MongoDB
-    await client.close();
+   const collections = [
+  'luz_ambiente',
+  'pressao',
+  'temperatura_ar',
+  'temperatura_solo',
+  'umidade_ar',
+  'umidade_solo',
+  'vento_direcao',
+  'vento_velocidade',
+  'heigth'
+];
 
-    const labels1 = data1.map(item => item.date);
-    const values1 = data1.map(item => item.data);
-    const labels2 = data2.map(item => item.date);
-    const values2 = data2.map(item => item.data);
-    const labels3 = data3.map(item => item.date);
-    const values3 = data3.map(item => item.data);
-   
+const data = {};
 
+for (let i = 0; i < collections.length; i++) {
+  const collection = client.db('data_sensor').collection(collections[i]);
+  data[`labels${i + 1}`] = (await collection.find({}).toArray()).map(item => item.date);
+  data[`values${i + 1}`] = (await collection.find({}).toArray()).map(item => item.data);
+}
 
-    // Responda com os dados em formato JSON
-    res.json({ labels1,values1, labels2,values2, labels3,values3});
+// Feche a conexão com o MongoDB
+await client.close();
+
+// Responda com os dados em formato JSON
+res.json(data);
+
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Erro ao buscar dados no MongoDB' });
